aca3730d1896a67c360510ea80a41c071891f12b
Jared Manning <jaredm@openmailbox.org>
2016-02-10 17:03:16 -0600

    Put count-change.scm in block structure.

--- sicp/ch1/1.2/count-change.scm
+++ sicp/ch1/1.2/count-change.scm
@@ -6,20 +6,22 @@
 ;;; that will be a challenge for later.
 ;;;
 ;;; As always, this isn't meant to be compiled. Run it in an interpreter.
+;;; 
+;;; I put this in block structure, but couldn't figure out the lexical scoping.
 
 (define (count-change amount)
+  (define (cc amount kinds-of-coins)
+    (cond ((= amount 0) 1)
+	  ((or (< amount 0) (= kinds-of-coins 0)) 0)
+	  (else (+ (cc amount
+		       (- kinds-of-coins 1))
+		   (cc (- amount
+			  (first-denomination kinds-of-coins))
+		       kinds-of-coins)))))
+  (define (first-denomination kinds-of-coins)
+    (cond ((= kinds-of-coins 1) 1)
+	  ((= kinds-of-coins 2) 5) 
+	  ((= kinds-of-coins 3) 10)
+	  ((= kinds-of-coins 4) 25)
+	  ((= kinds-of-coins 5) 50)))
   (cc amount 5))
-(define (cc amount kinds-of-coins)
-  (cond ((= amount 0) 1)
-	((or (< amount 0) (= kinds-of-coins 0)) 0)
-	(else (+ (cc amount
-		     (- kinds-of-coins 1))
-		 (cc (- amount
-			(first-denomination kinds-of-coins))
-		     kinds-of-coins)))))
-(define (first-denomination kinds-of-coins)
-  (cond ((= kinds-of-coins 1) 1)
-	((= kinds-of-coins 2) 5) 
-	((= kinds-of-coins 3) 10)
-	((= kinds-of-coins 4) 25)
-	((= kinds-of-coins 5) 50)))
