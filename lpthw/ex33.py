f46d7d930cd3deac9ab0987faf785fe832ad743c
Jared Manning <jaredm@openmailbox.org>
2016-05-27 06:50:49 -0500

    Study Drill (part 1) for ex33.py

--- lpthw/ex33.py
+++ lpthw/ex33.py
@@ -1,15 +1,42 @@
-i = 0
-numbers = []
+#i = 0
+#numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+#while i < 6:
+#    print "At the top i is %d" % i
+#    numbers.append(i)
+#
+#    i += 1
+#    print "Numbers now: ", numbers
+#    print "At the bottom i is %d" % i
+#
+#
+#print "The numbers: "
+#
+#for num in numbers:
+#    print num
 
-    i += 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+#Study Drills
+print "What's the limit of the list?"
+a = raw_input("> ")
 
-print "The numbers: "
+def list_numbers(a):
+    """This function might add numbers to a list?"""
+    i = 0
+    numbers = []
 
-for num in numbers:
-    print num
+    while i < a:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i += 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+    print "The numbers: "
+
+    for num in numbers:
+        print num
+
+    return
+
+list_numbers(a)
