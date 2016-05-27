54485df326b19612f501982f33b3a0a21f50b270
Jared Manning <jaredm@openmailbox.org>
2016-05-27 07:47:42 -0500

    Initial work for ex34.py.

---
+++ lpthw/ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "This is a test to see if I can name things on this list correctly."
+print "The animal at 1 is python:", 'python' == animals[1]
+print "The third animal is peacock:", 'peacock' == animals[2]
+print "The first animal is bear:", 'bear' == animals[0]
+print "The animal at 3 is kangroo:", 'kangaroo' == animals[3]
+print "The fifth animal is whale:", 'whale' == animals[4]
+print "The animal at 2 is peacock:", 'peacock' == animals[2]
+print "The sixth animal is platypus:", 'platypus' == animals[5]
+print "The animal at 4 is whale:", 'whale' == animals[4]
